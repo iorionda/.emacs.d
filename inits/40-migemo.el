dd630d274c3e94a7b2d677c7f41639412413b466
Iori ONDA <iori.onda@gmail.com>
2014-02-27 00:30:39 +0900

    Add settings

--- inits/40-migemo.el
+++ inits/40-migemo.el
@@ -4,13 +4,23 @@
 (require 'migemo)
 (when (and (executable-find "cmigemo")
            (require 'migemo nil t))
-  (setq migemo-options '("-q" "--emacs" "-i" "\g"))
+  (setq migemo-options '("-q" "--emacs"))
   (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
   (setq migemo-user-dictionary nil)
   (setq migemo-regex-dictionary nil)
-  (setq migemo-coding-system 'utf-8)
+  (setq migemo-coding-system 'utf-8-unix)
   (setq migemo-command "/usr/local/bin/cmigemo")
+  (load-library "migemo")
   (migemo-init))
+
+;; emacs 起動時は英数モードから始める
+(add-hook 'after-init-hook 'mac-change-language-to-us)
+
+;; minibuffer 内は英数モードにする
+(add-hook 'minibuffer-setup-hook 'mac-change-language-to-us)
+
+;; [migemo]isearch のとき IME を英数モードにする
+(add-hook 'isearch-mode-hook 'mac-change-language-to-us)
 
 (require 'helm-migemo)
 (setq helm-use-migemo t)
