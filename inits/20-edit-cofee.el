2b1387e8e0dcbc63b4c18e6752d3e97ba218c6e0
Iori ONDA <iori.onda@gmail.com>
2014-02-28 00:09:25 +0900

    Add settings

--- inits/20-edit-cofee.el
+++ inits/20-edit-cofee.el
@@ -3,6 +3,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;coffee-mode
 (require 'coffee-mode)
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("\\.Cakefile" . coffee-mode))
 
 ;; automatically clean up bad whitespace
 (setq whitespace-action '(auto-cleanup))
@@ -17,4 +19,17 @@
   (set (make-local-variable 'coffee-tab-width) 2))
 
 (add-hook 'coffee-mode-hook
+          (load-auto-complete)
           '(lambda() (coffee-custom)))
+
+(eval-after-load "coffee-mode"
+  '(progn
+     (define-key coffee-mode-map [(meta r)] 'coffee-compile-buffer)
+     (define-key coffee-mode-map (kbd "C-j") 'coffee-newline-and-indent)))
+
+(add-to-list 'load-path "~/.emacs.d/elisp/el-get/ac-coffee/")
+(require 'ac-coffee)
+(add-to-list 'ac-modes 'coffee-mode)
+(defun ac-coffee-mode-setup ()
+    (setq-default ac-sources my-ac-sources))
+(add-hook 'coffee-mode-hook 'ac-coffee-mode-setup)
