a87f8780351019a25edfc04dfb56c2092ef23b44
Iori ONDA <iori.onda@gmail.com>
2014-02-28 00:03:16 +0900

    Add ac-coffee

--- inits/10-el-get.el
+++ inits/10-el-get.el
@@ -1,15 +1,29 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 (setq el-get-dir "~/.emacs.d/elisp/el-get/")
-
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(add-to-list 'load-path "~/.emacs.d/elisp/el-get/el-get")
 
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
       (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
     (goto-char (point-max))
     (eval-print-last-sexp)))
+(el-get 'sync)
 
 ;;; recipe の格納場所
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
+(setq el-get-sources
+      '(
+        (:name ac-coffee
+               :type github
+               :description "open github from here"
+               :pkgname "akfish/ac-coffee"
+               :branch "master")
+        ))
+
+;; Packages to install from el-get
+(defvar my/el-get-packages
+  '(
+    ac-coffee
+    )
+  "A list of packages to install from el-get at launch.")
+(el-get 'sync my/el-get-packages)
