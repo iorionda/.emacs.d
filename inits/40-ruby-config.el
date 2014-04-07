3e4401a441d9461c08411b260eff3d2481aff52a
Iori ONDA <iori.onda@gmail.com>
2014-04-07 15:30:20 +0900

    Modify

--- inits/40-ruby-config.el
+++ inits/40-ruby-config.el
@@ -2,15 +2,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun execute-rspec ()
   (interactive)
-  (do-applescript (format "tell application \"iTerm\"
-  activate
-  tell current session of current terminal
-    write text \"bundle exec rspec %s:%s\"
-  end tell
-  end tell
-  tell application \"System Events\"
-    keystroke return
-  end tell"
-  buffer-file-name (line-number-at-pos))))
+  (do-applescript
+   (format
+    "tell application \"iTerm\"
+       activate
+       tell current session of current terminal
+         write text \"bundle exec rspec %s:%s\"
+       end tell
+     end tell"
+    buffer-file-name (line-number-at-pos))))
 
 (define-key ruby-mode-map (kbd "C-c r") 'execute-rspec)
