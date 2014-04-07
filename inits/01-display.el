8f2f1c07b8db4f50af8385442e99e06517bc89a4
Iori ONDA <iori.onda@gmail.com>
2014-04-07 15:11:58 +0900

    Change paren mode

--- inits/01-display.el
+++ inits/01-display.el
@@ -1,4 +1,4 @@
-;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
+;;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ディスプレイの設定
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -11,12 +11,15 @@
 (tool-bar-mode -1)
 
 ;; 起動時にフルスクリーンにする
-;; (add-hook 'window-setup-hook 'ns-toggle-fullscreen)
+(set-frame-parameter nil 'fullscreen 'fullboth)
 
 ;;;対応する括弧を光らせる
+(show-paren-mode 1)
 (setq show-paren-delay 0)
-(setq show-paren-style 'single)
-(show-paren-mode t)
+(setq show-paren-style 'expression)
+(set-face-attribute 'show-paren-match-face nil
+                    :background nil :foreground nil
+                    :underline "#ffff00" :weight 'extra-bold)
 
 ;;; ウィンドウ内に収まらないときだけ括弧内も光らせる。
 (setq show-paren-style 'mixed)
@@ -50,3 +53,29 @@
 (setq-default tab-width 4)
 (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60
                         64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))
+
+;====================================
+;;全角スペースとかに色を付ける
+;====================================
+(defface my-face-b-1 '((t (:background "SeaGreen"))) nil)
+(defface my-face-b-1 '((t (:background "SeaGreen"))) nil)
+(defface my-face-b-2 '((t (:background "SeaGreen"))) nil)
+(defface my-face-b-2 '((t (:background "SeaGreen"))) nil)
+(defface my-face-u-1 '((t (:foreground "SeaGreen" :underline t))) nil)
+(defvar my-face-b-1 'my-face-b-1)
+(defvar my-face-b-2 'my-face-b-2)
+(defvar my-face-u-1 'my-face-u-1)
+(defadvice font-lock-mode (before my-font-lock-mode ())
+  (font-lock-add-keywords
+   major-mode
+   '(
+     ("　" 0 my-face-b-1 append)
+     ("\t" 0 my-face-b-2 append)
+     ("[ ]+$" 0 my-face-u-1 append)
+     )))
+(ad-enable-advice 'font-lock-mode 'before 'my-font-lock-mode)
+(ad-activate 'font-lock-mode)
+(add-hook 'find-file-hooks '(lambda ()
+                              (if font-lock-mode
+                                  nil
+                                (font-lock-mode t))))
