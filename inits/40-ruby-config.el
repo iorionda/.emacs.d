0c1d72d0ed3dba3bf31e5a4ae5c3bbb74ca8db44
Iori ONDA <iori.onda@gmail.com>
2014-04-07 15:25:18 +0900

    Initial commit

---
+++ inits/40-ruby-config.el
@@ -0,0 +1,16 @@
+;;; 40-ruby-config.el --- for ruby-mode settings:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun execute-rspec ()
+  (interactive)
+  (do-applescript (format "tell application \"iTerm\"
+  activate
+  tell current session of current terminal
+    write text \"bundle exec rspec %s:%s\"
+  end tell
+  end tell
+  tell application \"System Events\"
+    keystroke return
+  end tell"
+  buffer-file-name (line-number-at-pos))))
+
+(define-key ruby-mode-map (kbd "C-c r") 'execute-rspec)
